83edd378e46ade429c1773d54ce991fb683d0635
SergioLabraLopez <slabralopez@gmail.com>
2025-04-07 12:35:41 +0200

    Agregación del proyecto Mongodb

---
+++ miapp-mongodb/src/main/java/com/ejemplo/miapp_mongodb/MiappMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.miapp_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiappMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiappMongodbApplication.class, args);
+	}
+
+}
